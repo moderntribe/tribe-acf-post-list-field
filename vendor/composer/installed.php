0b5afe950a7736892516e3a3d3524517d29eff92
Justin Frydman <justin.frydman@gmail.com>
2021-02-12 09:16:04 -0700

    Add missing files in vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.1',
+    'version' => '1.0.1.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'moderntribe/tribe-acf-post-list-field',
+  ),
+  'versions' => 
+  array (
+    'moderntribe/tribe-acf-post-list-field' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
