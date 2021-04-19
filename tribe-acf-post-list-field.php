5a9ca7cae12b5a0e2d909708d145b2e62d2cd376
Justin Frydman <justin.frydman@gmail.com>
2021-04-19 11:03:37 -0600

    bump to 2.2.0

--- tribe-acf-post-list-field.php
+++ tribe-acf-post-list-field.php
@@ -4,7 +4,7 @@
 Plugin Name: Advanced Custom Fields: Tribe Post List Field
 Plugin URI: https://tri.be
 Description: A post list field type for advanced custom fields
-Version: 2.1.4
+Version: 2.2.0
 Author: Modern Tribe
 Author URI: https://tri.be
 */
@@ -32,7 +32,7 @@ require_once $autoload;
 function tribe_acf_post_list(): void {
 
 	$settings = [
-		'version' => '2.1.4',
+		'version' => '2.2.0',
 		'url'     => plugin_dir_url( __FILE__ ),
 		'path'    => plugin_dir_path( __FILE__ ),
 	];
